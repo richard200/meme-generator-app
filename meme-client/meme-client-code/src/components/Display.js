658d2976cb31430968b955191e1179e7df73e7df
richard200 <richmn200@gmail.com>
2023-03-04 17:01:27 +0300

    working on display component

--- meme-client/meme-client-code/src/components/Display.js
+++ meme-client/meme-client-code/src/components/Display.js
@@ -1,45 +1,45 @@
-// import React, { useEffect, useState } from "react";
-// import MemeCollection from "./MemeCollection";
-// import NewMeme from "./NewMeme";
-// // import BotArmy from "./YourBotArmy";
+import React, { useEffect, useState } from "react";
+import MemeCollection from "./MemeCollection";
+import NewMeme from "./NewMeme";
+// import BotArmy from "./YourBotArmy";
 
-// function Display() {
-//   let [memes, setMemes] = useState([])
+function Display() {
+  let [memes, setMemes] = useState([])
 
-//   useEffect(() => {
-//     fetch("http://localhost:9292/memes")
-//       .then((resp) => resp.json())
-//       .then(data => setMemes(data))
-//   }, [])
+  useEffect(() => {
+    fetch("http://localhost:9292/memes")
+      .then((resp) => resp.json())
+      .then(data => setMemes(data))
+  }, [])
 
   
 
-// //   let addBot= (bot)=>{
-// //     setBot(bots.map((b)=>(b.id === bot.id ? {...b, army:true} : b)))
-// // }
-// // let removeBot =(bot)=>{
-// //     setBot(bots.map((b)=>(b.id === bot.id ? {...b, army:false} : b)))
-// // }
-
-// // let deleteBot = (bot)=>{
-// //     let botRemoved =  bots.filter((b)=> b.id !==bot.id);
-// //     setBot((bot)=>botRemoved)
-// // }
-//   return (
-//     <div>
-
-// <NewMeme/>
-//  <MemeCollection 
-//             memes= {memes}
-//             // addBot={addBot}
-//             // deleteBot={deleteBot}
-//  />
-
-
-
-
-//  </div>
-//   )
+//   let addBot= (bot)=>{
+//     setBot(bots.map((b)=>(b.id === bot.id ? {...b, army:true} : b)))
+// }
+// let removeBot =(bot)=>{
+//     setBot(bots.map((b)=>(b.id === bot.id ? {...b, army:false} : b)))
 // }
 
-// export default Display;
+// let deleteBot = (bot)=>{
+//     let botRemoved =  bots.filter((b)=> b.id !==bot.id);
+//     setBot((bot)=>botRemoved)
+// }
+  return (
+    <div>
+
+<NewMeme/>
+ <MemeCollection 
+            memes= {memes}
+            // addBot={addBot}
+            // deleteBot={deleteBot}
+ />
+
+
+
+
+ </div>
+  )
+}
+
+export default Display;
