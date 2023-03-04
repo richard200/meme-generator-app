89d3aa570ecca36e604f91ab49137ed245251f9b
richard200 <richmn200@gmail.com>
2023-03-04 16:52:47 +0300

    working on meme collection, memecard and new meme components

--- meme-client/meme-client-code/src/components/MemeCard.js
+++ meme-client/meme-client-code/src/components/MemeCard.js
@@ -1,84 +1,84 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 
-// function MemeCard({ meme}) {
-//     let [id, Setid] = useState("")
-// //       let [text_top, setTextTop] = useState("");
-// //   let [text_bottom, setTextBottom] = useState("");
+function MemeCard({ meme}) {
+    let [id, Setid] = useState("")
+//       let [text_top, setTextTop] = useState("");
+//   let [text_bottom, setTextBottom] = useState("");
 
 
-//     function handleDeleteClick(e) {
-//         e.preventDefault();
-//         fetch(`http://localhost:9292/memes/destroy/${meme.id}`, {
-//           method: "DELETE",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             id: id
+    function handleDeleteClick(e) {
+        e.preventDefault();
+        fetch(`http://localhost:9292/memes/destroy/${meme.id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: id
           
-//           }),
-//         }).then((r) => r.json())
-//         .then((data) => {
-//          Setid(id)
+          }),
+        }).then((r) => r.json())
+        .then((data) => {
+         Setid(id)
        
-//         });
+        });
     
     
-//       }
+      }
 
-//     //   function handleUpdateMeme(e) {
-//     //         e.preventDefault();
+    //   function handleUpdateMeme(e) {
+    //         e.preventDefault();
         
-//     //         fetch(`http://localhost:9292/memes/update/${meme.id}`, {
-//     //           method: "PATCH",
-//     //           headers: {
-//     //             "Content-Type": "application/json",
-//     //           },
-//     //           body: JSON.stringify({
-//     //             text_top: text_top,
-//     //             text_bottom: text_bottom
-//     //           }),
-//     //         })
-//     //         .then((r) => r.json())
-//     //         .then((data) => {
-//     //          setTextTop(text_top)
-//     //          setTextBottom(text_bottom)
-//     //         });
-//     //     }
+    //         fetch(`http://localhost:9292/memes/update/${meme.id}`, {
+    //           method: "PATCH",
+    //           headers: {
+    //             "Content-Type": "application/json",
+    //           },
+    //           body: JSON.stringify({
+    //             text_top: text_top,
+    //             text_bottom: text_bottom
+    //           }),
+    //         })
+    //         .then((r) => r.json())
+    //         .then((data) => {
+    //          setTextTop(text_top)
+    //          setTextBottom(text_bottom)
+    //         });
+    //     }
 
-//   return (
+  return (
 
-//  <div className="card col-6 mt-5 h-100 shadow p-3 mb-5 bg-body rounded"
-//  key={meme.id}>
-//     <div className="card-body" >
+ <div className="card col-6 mt-5 h-100 shadow p-3 mb-5 bg-body rounded"
+ key={meme.id}>
+    <div className="card-body" >
    
-//      {/* <h6 className="date">Name: {meme.name} </h6> */}
+     {/* <h6 className="date">Name: {meme.name} </h6> */}
         
-//      <p className="top_text">Top Text: {meme.text_top}</p>
+     <p className="top_text">Top Text: {meme.text_top}</p>
        
-//      <p className="category">Bottom Text: {meme.text_bottom}</p>
-//      <br>
-//      </br>
-//      <br></br>
+     <p className="category">Bottom Text: {meme.text_bottom}</p>
+     <br>
+     </br>
+     <br></br>
 
-//      <h5 className="category">Full Meme: {meme.text_top},  {meme.text_bottom}</h5>
+     <h5 className="category">Full Meme: {meme.text_top},  {meme.text_bottom}</h5>
 
-//      <button onClick={handleDeleteClick}>
-//             <span role="img" aria-label="delete">
-//               🗑
-//             </span>
-//           </button>
+     <button onClick={handleDeleteClick}>
+            <span role="img" aria-label="delete">
+              🗑
+            </span>
+          </button>
 
-//           {/* <button onClick={handleUpdateMeme}>
-//             <span role="img" aria-label="delete">
-//              Update
-//             </span>
-//           </button> */}
+          {/* <button onClick={handleUpdateMeme}>
+            <span role="img" aria-label="delete">
+             Update
+            </span>
+          </button> */}
 
-//   </div>
-//   </div>
-//    );
-//   }
+  </div>
+  </div>
+   );
+  }
 
-//   export default MemeCard;
+  export default MemeCard;
